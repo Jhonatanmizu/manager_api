c3dfcf65555a52abdd99dafaeea3b276796c631f
jhonatanmizu <natanjesuss20@gmail.com>
2024-11-16 13:12:08 -0300

    :fire: delete unused reminder repository

--- src/reminder/types/reminder.repository.ts
+++
@@ -1,6 +0,0 @@
-export interface ReminderRepository {
-  findAll: () => Promise<void>;
-  findOne: (id: string) => Promise<void>;
-  deleteOne: (id: string) => Promise<void>;
-  create: () => Promise<void>;
-}
