30a847252d18182045d02c22c15df00f0ddafd33
jhonatanmizu <natanjesuss20@gmail.com>
2026-01-22 23:05:42 -0300

    feat: create multer config

---
+++ src/config/multer.config.ts
@@ -0,0 +1,9 @@
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import multer from 'multer';
+
+export const multerConfig: MulterOptions = {
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10MB
+  },
+};
