7da43b551bbd5e7b5d3a23edb846f60976e4661b
jhonatanmizu <natanjesuss20@gmail.com>
2026-01-26 14:13:13 -0300

    feat: add new methods to abstract storage protocol

--- src/storage/storage.protocol.ts
+++ src/storage/storage.protocol.ts
@@ -1,8 +1,14 @@
 import { UploadResult } from './dtos/upload-result.dto';
 
-export interface StorageProtocol {
-  upload(file: Express.Multer.File, path?: string): Promise<UploadResult>;
-  delete(fileKey: string): Promise<void>;
-  getPublicUrl(fileKey: string): string;
-  getPresignedUrl(fileKey: string, expiresIn?: number): Promise<string>;
+export abstract class StorageProtocol {
+  abstract upload(
+    file: Express.Multer.File,
+    path?: string,
+  ): Promise<UploadResult>;
+  abstract delete(fileKey: string): Promise<void>;
+  abstract getPublicUrl(fileKey: string): string;
+  abstract getPresignedUrl(
+    fileKey: string,
+    expiresIn?: number,
+  ): Promise<string>;
 }
